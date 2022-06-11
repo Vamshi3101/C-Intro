43b7f6678b59d12ecfb8e315f6ce1dacf13326ff
Vamshi3101 <88844507+Vamshi3101@users.noreply.github.com>
2022-06-12 01:08:27 +0530

    Add files via upload

--- spiralTraversal.cpp
+++ spiralTraversal.cpp
@@ -1,39 +1,57 @@
 #include<iostream>
-#include<vector>
+#include<stack>
 using namespace std;
+struct Tree{
+    int val;
+    Tree *left;
+    Tree *right;
+    Tree(int x){
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+void SpiralTraversal(Tree *root){
+    if(root==NULL)return;
+    stack<Tree*>s1,s2;
+    s1.push(root);
+    bool flag = false;
+    while(!s1.empty()|| !s2.empty()){
+        if(flag){
+            int count = s2.size();
+            for(int i =0;i<count;i++){
+                Tree *temp = s2.top();
+                s2.pop();
+                cout<<temp->val<<" ";
+                if(temp->right!=NULL)s1.push(temp->right);
+                if(temp->left!=NULL)s1.push(temp->left);
+            }
+        }
+        else{
+            int count = s1.size();
+            for(int i =0;i<count;i++){
+                Tree *temp = s1.top();
+                s1.pop();
+                cout<<temp->val<<" ";
+                if(temp->left!=NULL)s2.push(temp->left);
+                if(temp->right!=NULL)s2.push(temp->right);
+            }
+        }
+        flag = !flag;
+    }
+}
 int main(){
-    int n;
-    cin>>n;
-    vector<vector<int>>v;
-    for(int i =0;i<n;i++){
-        vector<int>temp;
-        for(int j =0;j<n;j++){
-            int k;
-            cin>>k;
-            temp.push_back(k);
-        }
-        v.push_back(temp);
-    }
-    int t=0,l=0,r=n-1,b=n-1;
-    while (t<=b && l<=r)
-    {
-        for(int i = l;i<=r;i++){
-            cout<<v[t][i]<<" ";
-        }
-        t++;
-        for(int i = t;i<=b;i++){
-            cout<<v[i][r]<<" ";
-        }
-        r--;
-        for(int i = r;i>=l;i--){
-            cout<<v[b][i]<<" ";
-        }
-        b--;
-        for(int i = b;i>=t;i--){
-            cout<<v[i][l]<<" ";
-        }
-        l++;
-    }
-    
+    Tree *root = new Tree(1);
+    root->left = new Tree(2);
+    root->right = new Tree(3);
+    root->right->left = new Tree(6);
+    root->right->right = new Tree(7);
+    root->right->left->left = new Tree(10);
+    root->right->left->right = new Tree(11);
+    root->left->left = new Tree(4);
+    root->left->right = new Tree(5);
+    root->left->left->left = new Tree(8);
+    root->left->left->right = new Tree(9);
+    SpiralTraversal(root);
     return 0;
 }
